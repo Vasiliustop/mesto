38ac838314c58e58bfa0bed9a88656109f742607
Vasya <vasilgor@rambler.ru>
2021-12-25 16:54:49 +0300

    1try

---
+++ Scripts/Script.js
@@ -0,0 +1,51 @@
+
+const openPopup = document.querySelector('.profile__edit-button');
+const overActiveClass = 'overlay_active';
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.popup__close-button');
+const likeUse = document.querySelectorAll('.elements__element-button');
+const aktiveLike = 'elements__element-button_aktive';
+
+
+for(let i = 0; i < likeUse.length; i ++) {
+  likeUse[i].addEventListener('click', function() {
+  likeUse[i].classList.toggle(aktiveLike);
+ });
+}
+
+
+openPopup.addEventListener('click', function()
+{overlay.classList.add(overActiveClass);
+document.body.style.overflow = 'hidden';
+  profileName.value = profTitle.textContent;
+  profileJob.value = profSubtitle.textContent;
+});
+
+
+closeButton.addEventListener('click', function() {
+overlay.classList.remove(overActiveClass);
+document.body.style.overflow = '';
+});
+
+document.addEventListener('keydown', function(evt)
+  { if (evt.code === 'Escape') {
+    overlay.classList.remove(overActiveClass); }
+    document.body.style.overflow = '';
+});
+
+
+let profTitle = document.querySelector('.profile__title');
+let profSubtitle = document.querySelector('.profile__subtitle');
+let profileName = document.querySelector('.popup__name');
+let profileJob = document.querySelector('.popup__job');
+let formElement = document.querySelector('.popup__inputform');
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profTitle.textContent = profileName.value;
+  profSubtitle.textContent = profileJob.value;
+  overlay.classList.remove(overActiveClass);
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
