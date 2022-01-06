97396ea0d17c181b06b6269c4719a9ed08c91107
Vasya <vasilgor@rambler.ru>
2022-01-06 19:44:00 +0300

    2try

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -4,40 +4,32 @@ const overActiveClass = 'popup_active';
 const overlay = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 const likeUse = document.querySelectorAll('.elements__element-button');
-const aktiveLike = 'elements__element-button_aktive';
 
-
-for(let i = 0; i < likeUse.length; i ++) {
-  likeUse[i].addEventListener('click', function() {
-  likeUse[i].classList.toggle(aktiveLike);
- });
-}
-
-
-openPopup.addEventListener('click', function()
-{overlay.classList.add(overActiveClass);
-
+function openPop() {
+  overlay.classList.add(overActiveClass);
   profileName.value = profTitle.textContent;
   profileJob.value = profSubtitle.textContent;
-});
+};
+
+openPopup.addEventListener('click', openPop);
+
+function closePop() {
+  overlay.classList.remove(overActiveClass);
+  }
+
+closeButton.addEventListener('click', closePop);
 
 
-closeButton.addEventListener('click', function() {
-overlay.classList.remove(overActiveClass);
 
-});
-
-document.addEventListener('keydown', function(evt)
-  { if (evt.code === 'Escape') {
+document.addEventListener('keydown', function(evt) {
+  if (evt.code === 'Escape') {
     overlay.classList.remove(overActiveClass); }
-    
 });
-
 
 let profTitle = document.querySelector('.profile__title');
 let profSubtitle = document.querySelector('.profile__subtitle');
-let profileName = document.querySelector('.popup__name');
-let profileJob = document.querySelector('.popup__job');
+let profileName = document.querySelector('.popup__input_type_name');
+let profileJob = document.querySelector('.popup__input_type_job');
 let formElement = document.querySelector('.popup__inputform');
 
 
@@ -46,7 +38,6 @@ function formSubmitHandler (evt) {
   profTitle.textContent = profileName.value;
   profSubtitle.textContent = profileJob.value;
   overlay.classList.remove(overActiveClass);
-
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
